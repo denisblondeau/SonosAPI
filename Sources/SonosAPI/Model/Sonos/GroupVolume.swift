65119679bc10aba02826e72e70136da86f2d6bb4
Denis Blondeau <dbappdev@outlook.com>
2024-01-15 20:40:46 -0500

    Add Sonos GroupVolume object

---
+++ Sources/SonosAPI/Model/Sonos/GroupVolume.swift
@@ -0,0 +1,21 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let currentVolume = try? JSONDecoder().decode(CurrentVolume.self, from: jsonData)
+
+import Foundation
+
+// MARK: - GroupVolume
+struct GroupVolume: Codable {
+    public let currentVolume: Int
+
+    enum CodingKeys: String, CodingKey {
+        case currentVolume = "CurrentVolume"
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let value = try container.decode(String.self, forKey: .currentVolume)
+        currentVolume = Int(value) ?? 0
+    }
+}
